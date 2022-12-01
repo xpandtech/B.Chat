338862142f6474548e6460421add24cff4d34319
narjiraha <92223615+narjiraha@users.noreply.github.com>
2022-12-01 13:05:23 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Sidebar from './MyComponents/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello!
-        </p>
-
-      </header>
-    </div>
+   <Sidebar/>
   );
 }
 
