e0d5b64785d271386ec602d61334edb4eca71b35
Asbirvkp <116637800+asbirvkp@users.noreply.github.com>
2022-12-04 13:50:00 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Chatbox from './MyComponents/chatbox';
 import Navhead from './MyComponents/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Navhead/>
+      {/* <Navhead/> */}
+      <Chatbox/>
 
     </div>
   );
