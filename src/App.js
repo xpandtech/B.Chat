8f08cb1ff91f91760209aae36fcdf3613c214bec
sahusonali97 <113846876+sahusonali97@users.noreply.github.com>
2023-01-03 17:58:19 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+//import Navbar from './components/Navbar';
+import MainPage from './components/MainPage';
 
 
 const App = () => {
   return (
     <>
 
-    <Navbar/>
+    
+    <MainPage/>
 
     </>
   )
