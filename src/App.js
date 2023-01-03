f63817f415e6022e4ad88403469bd7beba1bc9af
Asbirvkp <116637800+asbirvkp@users.noreply.github.com>
2023-01-03 11:47:40 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import React from 'react';
-//import {Routes,Route} from 'react-router-dom'
-//import Inbox from './routes/Inbox'
-//import Broadcast from './routes/Broadcast';
-//import Outbox from './routes/Outbox';
-import Navbar from './componets/Navbar';
+
 
 
 function App() {
