382f7aaa3c0d8e03a8f075cc942b29f922663b07
sahusonali97 <113846876+sahusonali97@users.noreply.github.com>
2023-01-09 10:14:44 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import { ChatEngine } from 'react-chat-engine';
-
-import ChatFeed from './components/ChatFeed';
-import LoginForm from './components/LoginForm';
-import './App.css';
-
-const projectID = 'eec11bf8-0c40-4f33-a078-d09551198bc7';
-
+import React from "react";
+import MyChat from './Page/MyChat';
 const App = () => {
-  if (!localStorage.getItem('username')) return <LoginForm />;
+
 
   return (
-    <ChatEngine
-      height="100vh"
-      projectID={projectID}
-      userName={localStorage.getItem('username')}
-      userSecret={localStorage.getItem('password')}
-      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
-    />
+    <>
+    <MyChat/>
+    </>
+    
+    
   );
 };
+
 
 // infinite scroll, logout, more customizations...
 
---
+++ src/Page/MyChat.js
@@ -0,0 +1,29 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from '../components/ChatFeed';
+import LoginForm from '../components/LoginForm';
+import './MyChat.css';
+
+const projectID = 'eec11bf8-0c40-4f33-a078-d09551198bc7';
+
+const MyChat = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+    
+  );
+};
+
+
+// infinite scroll, logout, more customizations...
+
+export default MyChat;
