c4ae798b18f171e65435d98e4161ee6c546dd53f
sahusonali97 <113846876+sahusonali97@users.noreply.github.com>
2023-01-10 13:03:34 +0530

    update

--- src/components/Extranav.js
+++ src/components/Extranav.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import './ExtraNav.css';
 import Avatar from '@mui/material/Avatar';
+import SvgIcon from '@mui/material/SvgIcon';
+
 
 
 const Extranav = () => {
   return (
     <>
-    <div className='extranav'>
+      <div className='extnav'>
+      <span>
     <Avatar className="Avtar" alt="Travis Howard" src="https://mui.com/static/images/avatar/2.jpg" />
-    
+    <select className='Box'>
+      <option>Chat Bot</option>
+      <option>Chat Bot</option>
+      <option>Chat Bot</option>
+      <option>Chat Bot</option>
+      <option>Chat Bot</option>
+      <option>Chat Bot</option>
+      <option>Chat Bot</option>
+      <option>Chat Bot</option>
+    </select>
+    </span>
     </div>
     
     </>
@@ -16,4 +29,4 @@ const Extranav = () => {
   )
 }
 
-export default Extranav
+export default Extranav;
