227a36a1cf856500b4c6bb78425d52ab2bfd7e81
sahusonali97 <113846876+sahusonali97@users.noreply.github.com>
2023-01-11 18:51:04 +0530

    Revert "update"
    This reverts commit 9d69f9ed39a180ced098a7eb6f903d17c1a502c4.

--- src/components/subcomp/Timer.jsx
+++ src/components/subcomp/Timer.jsx
@@ -1,15 +1,24 @@
-import { CountdownCircleTimer } from 'react-countdown-circle-timer'
-import "./subcomp.css"
-const Timer = () => (
-  <div className='Time'>
-    <CountdownCircleTimer 
-    isPlaying
-    duration={60}
-    colors={['#004777', '#F7B801', '#A30000', '#A30000']}
-    colorsTime={[60, 45, 30, 10]}
-  >
-    {({ remainingTime }) => remainingTime}
-  </CountdownCircleTimer>
-  </div>
-)
-export default Timer;
+import React from 'react'
+import "./TimerStyle.css"
+const Timer = () => {
+    var countdownNumberEl = document.getElementById('countdown-number');
+    var countdown = 10;
+    
+    countdownNumberEl.textContent = countdown;
+    
+    setInterval(function() {
+      countdown = --countdown <= 0 ? 10 : countdown;
+    
+      countdownNumberEl.textContent = countdown;
+    }, 1000);  
+  return (
+<div id="countdown">
+  <div id="countdown-number"></div>
+  <svg>
+    <circle r="18" cx="20" cy="20"></circle>
+  </svg>
+</div>
+  )
+}
+
+export default Timer
