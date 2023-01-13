1a52aaf12de91b4bdd5195bb0d7778f6ffa85ec0
Asbirvkp <116637800+asbirvkp@users.noreply.github.com>
2023-01-13 19:22:16 +0530

    update

---
+++ src/components/Extranav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './ExtraNav.css';
+import Agent from './subcomponents.jsx/Agent';
+
+
+
+const Extranav = () => {
+  return (
+    <>
+      <div className='extnav'>
+      <Agent/>
+
+    </div>
+    
+    </>
+    
+  )
+}
+
+export default Extranav;
