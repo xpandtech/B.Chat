e4355f37fa894b18529122d9aeb796544be2dbb6
sahusonali97 <113846876+sahusonali97@users.noreply.github.com>
2023-02-02 19:31:30 +0530

    updated

--- src/components/Chat/ChatSettings/ChatSettingsColumn.js
+++ src/components/Chat/ChatSettings/ChatSettingsColumn.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import Avatar from '@mui/material/Avatar';
-
 const ChatSettingsColumn = () => {
   return (
-    <div>
-      <Avatar alt="Travis Howard" src="https://mui.com/static/images/avatar/2.jpg" />
+    <div className='ce-settings-column'>
+      <Avatar  className='avt' alt="Travis Howard" src="https://mui.com/static/images/avatar/2.jpg" />
 
     </div>
   )
 }
 
-export default ChatSettingsColumn
+export default ChatSettingsColumn;
