9eacd87ea48bb0d7be094c184955b1ee9969138f
Asbirvkp <116637800+asbirvkp@users.noreply.github.com>
2023-02-03 10:41:42 +0530

    Revert "updated"
    This reverts commit e4355f37fa894b18529122d9aeb796544be2dbb6.

--- src/components/Chat/ChatSettings/ChatSettingsColumn.js
+++ src/components/Chat/ChatSettings/ChatSettingsColumn.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Avatar from '@mui/material/Avatar';
+
 const ChatSettingsColumn = () => {
   return (
-    <div className='ce-settings-column'>
-      <Avatar  className='avt' alt="Travis Howard" src="https://mui.com/static/images/avatar/2.jpg" />
+    <div>
+      <Avatar alt="Travis Howard" src="https://mui.com/static/images/avatar/2.jpg" />
 
     </div>
   )
 }
 
-export default ChatSettingsColumn;
+export default ChatSettingsColumn
