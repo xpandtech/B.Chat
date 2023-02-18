df56def4979adb1b811d5c6465cb3bd314e57368
Chinmaya Sahoo <106534765+Chinmaya255696@users.noreply.github.com>
2023-02-18 20:35:27 +0530

    chatfeed header

--- src/components/Chat/ChatFeed/HeaderComp/SelectInput.js
+++ src/components/Chat/ChatFeed/HeaderComp/SelectInput.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
+import { DotsVerticalRounded } from "@styled-icons/boxicons-regular/DotsVerticalRounded";
+import { Robot } from"@styled-icons/fa-solid/Robot"
 const SelectInput = () => {
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
 
   const handleChange = (event) => {
     console.log(`Selected ${event.target.value}`);
@@ -9,16 +10,19 @@ const SelectInput = () => {
   };
 
   return (
-    <div className="select-operator">
-      <select value={value} onChange={handleChange}>
-        <option value="">Select Operator</option>
-        <option value="jack">Jack</option>
-        <option value="lucy">Lucy</option>
-        <option value="tom">Tom</option>
-      </select>
-    </div>
+    <>
+      <div className="select-operator">
+        <select value={value} onChange={handleChange}>
+          <option value="">Select Operator</option>
+          <option value="jack">Jack</option>
+          <option value="lucy">Lucy</option>
+          <option value="tom">Tom</option>
+        </select>
+      </div>
+      <Robot className="robot"/>
+        <DotsVerticalRounded className="dotsVerticalRounded"/>
+    </>
   );
 };
 
 export default SelectInput;
-
