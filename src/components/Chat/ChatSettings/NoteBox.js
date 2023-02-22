576d1e9cf76dad2b3841ec783087b2f1d3a90ff0
sahusonali97 <113846876+sahusonali97@users.noreply.github.com>
2023-02-22 18:01:19 +0530

    updated

---
+++ src/components/Chat/ChatSettings/NoteBox.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+// import "./styles.css";
+// import { RiDeleteBin6Fill } from 'react-icons/ri';
+
+ 
+const NoteBox = () => {
+  const [value, setValue] = useState("");
+  const [date, setDate] = useState("");
+
+  const handleInputChange = (event) => {
+    setValue(event.target.value);
+    setDate(new Date().toLocaleString());
+  };
+
+  const handleDeleteClick = () => {
+    setValue("");
+    setDate("");
+  };
+
+  return (
+    <div className="input-container">
+        
+      <input
+        type="text"
+        value={value}
+        onChange={handleInputChange}
+        placeholder="Type something..."
+      />
+      {value && (
+        <div className="delete-icon" onClick={handleDeleteClick}>
+          &#10005;
+
+        </div>
+      )}
+      <div className="date">{date}</div>
+
+    </div>
+  );
+};
+
+export default NoteBox;
