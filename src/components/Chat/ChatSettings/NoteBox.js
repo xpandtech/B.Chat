33c8f9b7b3ed4f746ab7496ed07a57467b7ee8cd
Asbirvkp <116637800+asbirvkp@users.noreply.github.com>
2023-02-27 11:58:38 +0530

    Revert "update"
    This reverts commit 011e0c8542e3288cffd39e280571d3e19a7725c4, reversing
    changes made to 06a8dbb44e5793f80fd7d176ae5fd6d74574dc8f.

--- src/components/Chat/ChatSettings/NoteBox.js
+++ src/components/Chat/ChatSettings/NoteBox.js
@@ -1,140 +1,29 @@
 import React from "react";
-import {
-  MDBCard,
-  MDBCardBody,
-  MDBCardImage,
-  MDBCol,
-  MDBContainer,
-  MDBIcon,
-  MDBInput,
-  MDBRow,
-} from "mdb-react-ui-kit";
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-import "@fortawesome/fontawesome-free/css/all.min.css";
+import { CommentSection } from "react-comments-section";
+import "react-comments-section/dist/index.css";
+// import NoteBox from './NoteBox';
 
-export default function NoteBox() {
+const NoteBox = () => {
+  const data = [
+    {
+      userId: "02b",
+      comId: "017",
+      replies: []
+    }
+  ];
   return (
-    <MDBContainer className="mt-5" style={{ maxWidth: "1000em" }}>
-      <MDBRow className="justify-content-center">
-        <MDBCol md="8" lg="6">
-          <MDBCard
-            className="shadow-0 border"
-            style={{ backgroundColor: "#f0f2f5" }}
-          >
-            <MDBCardBody>
-              <MDBInput wrapperClass="mb-4" placeholder="Type comment..." label="+ Add a note" />
-
-              <MDBCard className="mb-4">
-                <MDBCardBody>
-                  <p>Type your note, and hit enter to add it</p>
-
-                  <div className="d-flex justify-content-between">
-                    <div className="d-flex flex-row align-items-center">
-                      <MDBCardImage
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(4).webp"
-                        alt="avatar"
-                        width="25"
-                        height="25"
-                      />
-                      <p className="small mb-0 ms-2">Martha</p>
-                    </div>
-                    <div className="d-flex flex-row align-items-center">
-                      <p className="small text-muted mb-0">Upvote?</p>
-                      <MDBIcon
-                        far
-                        icon="thumbs-up mx-2 fa-xs text-black"
-                        style={{ marginTop: "-0.16rem" }}
-                      />
-                      <p className="small text-muted mb-0">3</p>
-                    </div>
-                  </div>
-                </MDBCardBody>
-              </MDBCard>
-
-              <MDBCard className="mb-4">
-                <MDBCardBody>
-                  <p>Type your note, and hit enter to add it</p>
-
-                  <div className="d-flex justify-content-between">
-                    <div className="d-flex flex-row align-items-center">
-                      <MDBCardImage
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(32).webp"
-                        alt="avatar"
-                        width="25"
-                        height="25"
-                      />
-                      <p className="small mb-0 ms-2">Johny</p>
-                    </div>
-                    <div className="d-flex flex-row align-items-center">
-                      <p className="small text-muted mb-0">Upvote?</p>
-                      <MDBIcon
-                        far
-                        icon="thumbs-up mx-2 fa-xs text-black"
-                        style={{ marginTop: "-0.16rem" }}
-                      />
-                      <p className="small text-muted mb-0">4</p>
-                    </div>
-                  </div>
-                </MDBCardBody>
-              </MDBCard>
-
-              <MDBCard className="mb-4">
-                <MDBCardBody>
-                  <p>Type your note, and hit enter to add it</p>
-
-                  <div className="d-flex justify-content-between">
-                    <div className="d-flex flex-row align-items-center">
-                      <MDBCardImage
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(31).webp"
-                        alt="avatar"
-                        width="25"
-                        height="25"
-                      />
-                      <p className="small mb-0 ms-2">Mary Kate</p>
-                    </div>
-                    <div className="d-flex flex-row align-items-center text-primary">
-                      <p className="small mb-0">Upvoted</p>
-                      <MDBIcon
-                        fas
-                        icon="thumbs-up mx-2 fa-xs"
-                        style={{ marginTop: "-0.16rem" }}
-                      />
-                      <p className="small mb-0">2</p>
-                    </div>
-                  </div>
-                </MDBCardBody>
-              </MDBCard>
-
-              <MDBCard className="mb-4">
-                <MDBCardBody>
-                  <p>Type your note, and hit enter to add it</p>
-
-                  <div className="d-flex justify-content-between">
-                    <div className="d-flex flex-row align-items-center">
-                      <MDBCardImage
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(32).webp"
-                        alt="avatar"
-                        width="25"
-                        height="25"
-                      />
-                      <p className="small mb-0 ms-2">Johny</p>
-                    </div>
-                    <div className="d-flex flex-row align-items-center">
-                      <p className="small text-muted mb-0">Upvote?</p>
-                      <MDBIcon
-                        far
-                        icon="thumbs-up mx-2 fa-xs text-black"
-                        style={{ marginTop: "-0.16rem" }}
-                      />
-                      <p className="small text-muted mb-0"></p>
-                    </div>
-                  </div>
-                </MDBCardBody>
-              </MDBCard>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
+    <CommentSection
+      currentUser={{
+        currentUserImg:
+          "https://ui-avatars.com/api/name=Riya&background=random",
+      }}
+      logIn={{
+        loginLink: "http://localhost:3001/",
+        signupLink: "http://localhost:3001/"
+      }}
+      commentData={data}
+    />
   );
-}
+};
+
+export default NoteBox;
