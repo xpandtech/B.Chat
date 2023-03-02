d870d3648b21391fb0cc305a6ac70ed5d2586afd
Mahammed Sarfaraz Nawaz Pagala <72764237+pmdnawaz1@users.noreply.github.com>
2023-03-02 17:09:32 +0530

    Update NotesList.js

--- src/components/Chat/ChatSettings/NoteBox/components/NotesList/NotesList.js
+++ src/components/Chat/ChatSettings/NoteBox/components/NotesList/NotesList.js
@@ -2,10 +2,40 @@ import React from 'react';
 import './NotesList.css';
 import { FaTrashAlt, FaPencilAlt } from 'react-icons/fa';
 
+class NoteQueue {
+  constructor() {
+    this.notes = [];
+  }
+
+  add(note) {
+    this.notes.push(note);
+  }
+
+  remove() {
+    return this.notes.shift();
+  }
+
+  isEmpty() {
+    return this.notes.length === 0;
+  }
+
+  reverse() {
+    this.notes.reverse();
+  }
+}
+
 const notesList = ({notes, editNote, deleteNote}) => {
-  return notes.length ? (
-    notes.map(note => <div className="note" key={note.id}>{note.note}<span className="note-edit" onClick={() => editNote(note.id)}><FaPencilAlt /></span><span className="note-delete" onClick={() => deleteNote(note.id)}><FaTrashAlt /></span></div>)
-  ) : null
+  const queue = new NoteQueue();
+  notes.forEach(note => queue.add(note));
+  queue.reverse();
+  
+  const noteElements = [];
+  while (!queue.isEmpty()) {
+    const note = queue.remove();
+    noteElements.push(<div className="note" key={note.id}>{note.note}<span className="note-edit" onClick={() => editNote(note.id)}><FaPencilAlt /></span><span className="note-delete" onClick={() => deleteNote(note.id)}><FaTrashAlt /></span></div>);
+  }
+  
+  return noteElements.length ? noteElements : null;
 }
 
 
